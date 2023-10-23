39dd2a6731e7014abe2789701ef4184bba4690ea
Sam Potter <spotter1642@gmail.com>
2023-10-23 04:53:33 -0500

    Add budget template

--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
     "fmt"
+    "html/template"
     "log"
     "net/http"
 )
@@ -12,7 +13,13 @@ func hello(w http.ResponseWriter, req *http.Request) {
     fmt.Fprint(w, "hello\n")
 }
 
+func budget(w http.ResponseWriter, req *http.Request) {
+    tmpl := template.Must(template.ParseFiles("templates/budget.html"))
+    tmpl.Execute(w, nil)
+}
+
 func main() {
     http.HandleFunc("/hello", hello)
+    http.HandleFunc("/budget", budget)
     log.Fatal(http.ListenAndServe(fmt.Sprintf(":%d", port), nil))
 }
