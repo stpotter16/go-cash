d98406fbe7f03d78e5518b6de51b01feeaf56b83
Sam Potter <spotter1642@gmail.com>
2023-10-24 04:49:22 -0500

    Embed and parse templates

--- main.go
+++ main.go
@@ -1,10 +1,20 @@
 package main
 
 import (
-    "fmt"
-    "html/template"
-    "log"
-    "net/http"
+	"embed"
+	"fmt"
+	"html/template"
+    "io/fs"
+	"log"
+	"net/http"
+)
+
+var (
+    //go:embed all:templates/*
+    templatesFS embed.FS
+
+    //the parsed templates
+    html *template.Template
 )
 
 const port = 8080
@@ -14,11 +24,16 @@ func hello(w http.ResponseWriter, req *http.Request) {
 }
 
 func budget(w http.ResponseWriter, req *http.Request) {
-    tmpl := template.Must(template.ParseFiles("templates/budget.html"))
-    tmpl.Execute(w, data)
+    html.ExecuteTemplate(w, "budget.html", data)
+}
+
+func parseTemplates (templates fs.FS) (*template.Template) {
+    parsed := template.Must(template.New("").ParseFS(templates, "templates/*.html"))
+    return parsed
 }
 
 func main() {
+    html = parseTemplates(templatesFS)
     http.HandleFunc("/hello", hello)
     http.HandleFunc("/budget", budget)
     log.Fatal(http.ListenAndServe(fmt.Sprintf(":%d", port), nil))
