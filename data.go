9f22d64fc8b59d65e2b624f9fdfb6ea1b351d556
Sam Potter <spotter1642@gmail.com>
2023-10-24 05:14:28 -0500

    Give budgets a name

--- data.go
+++ data.go
@@ -1,6 +1,12 @@
 package main
 
-var data []Category
+var data Budget
+
+type Budget struct {
+    ID string
+    Name string
+    Categories []Category
+}
 
 type Category struct {
     ID string
@@ -8,7 +14,7 @@ type Category struct {
 }
 
 func init() {
-    data = []Category{
+    categories := []Category{
         {
             ID: "1",
             Name: "Income",
@@ -17,5 +23,10 @@ func init() {
             ID: "2",
             Name: "Housing",
         },
+    }
+    data = Budget{
+        ID: "1",
+        Name: "October",
+        Categories: categories,
     }
 }
