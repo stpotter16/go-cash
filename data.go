dfc3c53c317b2adcfa1e68385eafdc5b0f8bc960
Sam Potter <spotter1642@gmail.com>
2023-10-25 05:38:11 -0500

    Add some notion of budget items

--- data.go
+++ data.go
@@ -11,19 +11,50 @@ type Budget struct {
 type Category struct {
     ID string
     Name string
+    Items []BudgetItem
+}
+
+type BudgetItem struct {
+    ID string
+    Name string
+    Amount float64
 }
 
 func init() {
+    income := []BudgetItem{
+        {
+            ID: "1",
+            Name: "Paycheck 1",
+            Amount: 10000,
+        },
+        {
+            ID: "2",
+            Name: "Paycheck 2",
+            Amount: 10000,
+        },
+    }
+
+    housing := []BudgetItem{
+        {
+            ID: "1",
+            Name: "Mortgage",
+            Amount: 3000,
+        },
+    }
+
     categories := []Category{
         {
             ID: "1",
             Name: "Income",
+            Items: income,
         },
         {
             ID: "2",
             Name: "Housing",
+            Items: housing,
         },
     }
+
     data = Budget{
         ID: "1",
         Name: "October",
